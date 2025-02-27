6385de970a30384aadb0261693a187f88ff97017
Al-khudri <mohammad.alkhudri@outlook.dk>
2025-02-27 12:52:32 +0100

    C and R in Crud for employees
    Crud operations

--- src/main/java/com/example/haarmonika/Datarepo.java
+++ src/main/java/com/example/haarmonika/Datarepo.java
@@ -1,9 +1,56 @@
 package com.example.haarmonika;
+import com.example.haarmonika.Model.Customer;
+import com.example.haarmonika.Model.Employee;
+
+import javax.swing.*;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 public class Datarepo {
-    public void creatCustomer(){
+    //C i Crud Creat employee
+    public void creatEmployee(Employee employee){
+        String sql = "insert into employee (LastName,FirstName,Email,PhoneNumber,Password) values (?,?,?,?,?)";
+
+        try (Connection connection = Databaseconnection.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+
+            preparedStatement.setString(1, employee.getLastName());
+            preparedStatement.setString(2, employee.getFirstName());
+            preparedStatement.setString(3, employee.getPhoneNumber());
+            preparedStatement.setString(4,employee.getEmail());
+            preparedStatement.setString(5, employee.getPassword());
+
+            int rowsInserted = preparedStatement.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("employee has been created");
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
+    //R u Crud for at read employees
+    public  List<Employee> getAllEmployee(){
+        List<Employee> employees = new ArrayList<>();
+        String sql = "select * from employee";
+
+        try (Connection connection = Databaseconnection.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)){
+
+            while(resultSet.next()){
+                int Id =resultSet.getInt("Id");
+                String LastName = resultSet.getString("LastName");
+                String FirstName = resultSet.getString("FirstName");
+                String Email = resultSet.getString("Email");
+                String PhoneNumber = resultSet.getString("PhoneNumber");
+                String Password = resultSet.getString("Password");
+                employees.add(new Employee(Id,LastName,FirstName,Email,PhoneNumber,Password));
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return employees;
+    }
 }
+
--- src/main/java/com/example/haarmonika/Model/Employee.java
+++ src/main/java/com/example/haarmonika/Model/Employee.java
@@ -6,5 +6,47 @@ public class Employee extends Person {
         super(id, LastName, FirstName, email, phoneNumber, password);
     }
 
+    @Override
+    public int getId() {
+        return super.getId();
+    }
+
+    @Override
+    public void setId(int id) {
+        super.setId(id);
+    }
+
+    @Override
+    public String getLastName() {return super.getLastName();}
+
+    @Override
+    public void setLastName(String LastName) {super.setLastName(LastName);}
+
+    @Override
+    public String getFirstName() {return super.getFirstName();}
+
+    @Override
+    public void setFirstName(String FirstName) {super.setFirstName(FirstName);}
+
+    @Override
+    public String getEmail() {return super.getEmail();}
+
+    @Override
+    public void setEmail(String email) {super.setEmail(email);}
+
+    @Override
+    public String getPhoneNumber() {return super.getPhoneNumber();}
+
+    @Override
+    public void setPhoneNumber(String phoneNumber) {super.setPhoneNumber(phoneNumber);}
+
+    @Override
+    public String getPassword() {return super.getPassword();}
+
+    @Override
+    public void setPassword(String password) {super.setPassword(password);}
+
+
+
 
 }
